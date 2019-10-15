9c075e5c40c06046a4ee13a2d22ebbc767e03f12
sandraottersten <otterstensandra@gmail.com>
2019-10-15 19:37:27 +0200

    applied grid to cardText

--- src/components/MenuComponents/Card/CardText.vue
+++ src/components/MenuComponents/Card/CardText.vue
@@ -1,16 +1,18 @@
 <template> 
   <div class='content-text-wrapper'>
-    <div class='dish-title'>
       <div class='dish-name'>
         <h3>{{item.name}}</h3>
-        <div class="line" />
       </div>
-      <h4>{{item.price}}:-</h4>
-    </div>
+      <div class='dish-price'>
+        <h4>{{item.price}}:-</h4>
+      </div>
+      <div class='dish-number'>
+        <h4>1</h4>
+      </div>
     <div class='dish-content'>
-      <h4>1</h4>
       <p>{{item.description}}</p>
     </div>
+    <div class="content-filler" />
     <Icons :proteinItems="item.protein"/>
   </div>
 </template>
