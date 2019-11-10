b71e808b57c8d51bb24056566c98375a351e223b
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-11-10 13:26:53 +0100

    Added content for use on flight. Also added new drink-info for the new grid-layout

--- src/components/MenuComponents/DrinkCard.vue
+++ src/components/MenuComponents/DrinkCard.vue
@@ -2,17 +2,21 @@
   <div class="drink-card">
     <section class="drink-text">
       <div class="number">
-        <h4>{{drink.drinkNr}}</h4>  
+        <h4>{{test.drinkNr}}</h4>  
       </div>
       <div class="drink-name">
-        <h3>{{drink.drinkName}}</h3>
+        <h3>{{test.drinkName}}</h3>
       </div>
       <div class="price">
-        <h4>{{drink.price}}</h4>
+        <h4>{{test.price}}</h4>
       </div>
       <div class="add-icon">
         <Add class="add-icon"/>
       </div>
+      <div class="drink-info">
+        <p>{{test.info}}</p>
+      </div>
+      <div class="filler" />
     </section>
   </div>
 </template>
@@ -24,10 +28,20 @@ export default {
   components: {
     Add,
   },
+  data() {
+    return {
+      test: {
+        price: 33,
+        drinkName: 'Cola Cola',
+        drinkNr: 3,
+        info: '33cl',
+      } 
+    }
+  },
   props: {
     drink: {
       type: Object,
-      required: true,
+      required: false,
     },
   },
 };
