2fc5a224787e8645ba1a7eac45f6bb57b8462f06
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-11-14 03:03:35 +0100

    Cleaned up code

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,12 +1,10 @@
 export default {
-    getOrderItem (state) {
-      return (name) => {
-        return state.menuItems.filter(
-          item => item.productName == name
-        )[0]
-      }
-    },
-    sortedMenuItems(state) {
-      return state.menuItems.sort((a, b) => (a.category > b.category) ? 1 : -1)
-    }
+  getOrderItem(state) {
+    return name => state.menuItems.filter(
+      item => item.productName === name,
+    )[0];
+  },
+  sortedMenuItems(state) {
+    return state.menuItems.sort((a, b) => ((a.category > b.category) ? 1 : -1));
+  },
 };
