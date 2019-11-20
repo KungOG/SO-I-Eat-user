436a29ecab3a8b5ff30139a4bb1b9ffc42776a2f
sandraottersten <otterstensandra@gmail.com>
2019-11-20 13:17:07 +0100

    refactor image data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
     <div class='wrapper' :style="{backgroundImage: 'url(' + getImgUrl() + ')'}">
       <NavigationBar class="mobile"/>
       <section class="navigation-container">
-        <NavigationButton :imageSrc="TakeAway" :title="TakeAwayText"/>
-        <NavigationButton :imageSrc="MenuIcon" :title="MenuIconText"/>
-        <NavigationButton :imageSrc="EatHere" :title="EatHereText" class="mobile"/>
-        <NavigationButton :imageSrc="Maps" :title="MapsText" class="desktop"/>
+        <NavigationButton :imageSrc="TakeAway.url" :title="TakeAway.text"/>
+        <NavigationButton :imageSrc="MenuIcon.url" :title="MenuIcon.text"/>
+        <NavigationButton :imageSrc="EatHere.url" :title="EatHere.text" class="mobile"/>
+        <NavigationButton :imageSrc="Maps.url" :title="Maps.text" class="desktop"/>
       </section>
       <div class='golden-arrow'>
-        <img :src="require('@/assets/icons/'+ Arrow)" alt="" @click="test()">
+        <img :src="Arrow" alt="" @click="test()">
       </div>
     </div>
     <Menu />
@@ -29,24 +29,16 @@ export default {
     NavigationButton,
   },
   data: () => ({
-    TakeAway: require('@/assets/icons/TakeAway.svg'),
-    TakeAwayText: 'Ta med',
-    MenuIcon: require('@/assets/icons/MenuIcon.svg'),
-    MenuIconText: 'Se Menyn',
-    Maps: require('@/assets/icons/Maps.svg'),
-    MapsText: 'Hitta hit',
-    EatHere: require('@/assets/icons/EatHere.svg'),
-    EatHereText: 'Äta här',
-    Logga: 'logga.svg',
-    Maps: 'Maps.svg',
-    Info: 'Info.svg',
-    Arrow: 'Arrow.svg',
+    TakeAway: {url: require('@/assets/icons/TakeAway.svg'), text: 'Ta med'},
+    MenuIcon: {url: require('@/assets/icons/MenuIcon.svg'), text: 'Se Menyn'} ,
+    Maps: {url: require('@/assets/icons/Maps.svg'), text: 'Hitta hit'},
+    EatHere: {url: require('@/assets/icons/EatHere.svg'), text: 'Äta här'},
+    Arrow: require('@/assets/icons/Arrow.svg'),
   }),
   methods: {
     test () {
       console.log('click');
     },
-
     getImgUrl() {
       return require('@/assets/images/background.png')
     },
