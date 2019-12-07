b24afc78804d8abbfc5b4eeb21108977c6d8722b
sandraottersten <otterstensandra@gmail.com>
2019-12-07 20:39:41 +0100

    added info text to contact

--- src/components/info/Contact.vue
+++ src/components/info/Contact.vue
@@ -1,6 +1,12 @@
 <template>
     <div class='contact'>
       <Map />
+      <div class="text-wrapper">
+        <p>Öppet tis-sön 11-21 <br> Lunch tis-fre 11-15</p>
+        <hr>
+        <p>Kustvägen 46 <br> 312 60 Mellbystrand</p>
+        <p>+46 430 28300</p>
+      </div>
     </div>
 </template>
 <script>
