ec777952d8659e8756b3626bb877d25d21cf4ac9
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-12-08 13:01:58 +0100

    Added and moved about, contact and terms. Also added content inside of all of them

---
+++ src/components/InfoComponents/InfoHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='info-header'>
+    <div class='icon-one'>x</div>
+    <div class='icon-two' :class="showBurgerNav ? '-active' : ''" @click="showBurger">C</div>
+    <div class='burger-menu' :class="showBurgerNav ? '-active' : ''">
+      <div class='burger-container'>
+        <div @click="showBurger">X</div>
+        <span>About</span>
+        <span>Contact</span>
+        <span>Terms</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'info-header',
+  data: () => ({
+    showBurgerNav: false,
+  }),
+  methods: {
+    showBurger() {
+      console.log(this.showBurgerNav);
+      this.showBurgerNav = !this.showBurgerNav;
+    }
+  }
+}
+</script>
