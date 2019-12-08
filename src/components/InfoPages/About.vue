25a7374501dcb893142278ad2fa8117b077ef5ab
sandraottersten <otterstensandra@gmail.com>
2019-12-08 13:15:30 +0100

    added basic content to about

--- src/components/InfoPages/About.vue
+++ src/components/InfoPages/About.vue
@@ -1,5 +1,12 @@
  <template>
   <div class='about'>
-    <h1>This is an about page</h1>
+    <section class="about-img-wrapper">
+      Bild
+    </section>
+    <section class="about-text-wrapper">
+      <h3>Genuina smaker från Thailand</h3>
+      <hr>
+      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </section>
   </div>
 </template>
