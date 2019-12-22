e9e6da625fb0bd6ca1b891146bd1cb5710e90eb6
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-12-22 15:31:38 +0100

    FIX: ESLint Icons

--- src/components/MenuComponents/Card/Icons.vue
+++ src/components/MenuComponents/Card/Icons.vue
@@ -1,6 +1,10 @@
 <template>
   <div class='icon-wrapper'>
-    <img class='protein-icon' v-for="(item, i) in proteinItems" :key="i" v-if="item !== ''" :src="require('@/assets/icons/' + item +'.svg')" alt="item.name">
+    <img class='protein-icon'
+      v-for="(item, i) in proteinItems"
+      :key="i" v-if="item !== ''"
+      :src="require('@/assets/icons/' + item +'.svg')"
+      alt="item.name">
   </div>
 </template>
 
