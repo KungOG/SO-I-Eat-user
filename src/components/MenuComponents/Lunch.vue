6e9cc247293c223ee8b3e1ce0ea6869f9cb3f559
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-12-22 15:30:57 +0100

    FIX: ESLint Lunch

--- src/components/MenuComponents/Lunch.vue
+++ src/components/MenuComponents/Lunch.vue
@@ -12,36 +12,31 @@
 </template>
 
 <style lang="scss">
-  .lunch-section {
-    background: white;
+.lunch-section {
+  background: white;
+  padding: 1.5rem;
+  margin-bottom: .7rem;
+
+  @media (min-width: 992px) {
+    padding: 2.5rem 4rem;
+  }
+
+  .lunch-content {
+    border: 1px solid #c4c4c4;;
     padding: 1.5rem;
-    margin-bottom: .7rem;
 
-    @media (min-width: 992px) {
-      padding: 2.5rem 4rem;
+    p:nth-child(2) {
+      margin-top: 10px;
     }
 
-    .lunch-content {
-      border: 1px solid #c4c4c4;;
-      padding: 1.5rem;
+    div {
+      display: flex;
+      justify-content: space-between;
 
-      p:nth-child(2) {
-        margin-top: 10px;
-      }
-
-      div {
-        display: flex;
-        justify-content: space-between;
-
-        h3 {
-          line-height: 1;
-        }
-
-        h4 {
-          line-height: 1;
-        }
-
+      h3, h4 {
+        line-height: 1;
       }
     }
-  } 
+  }
+}
 </style>
