264c781b05f9533c53f326b6d00ef186d113facb
sandraottersten <otterstensandra@gmail.com>
2019-12-28 11:56:48 +0100

    commented back some code

--- src/store/getters.js
+++ src/store/getters.js
@@ -7,11 +7,11 @@ export default {
   sortedMenuItems(state) {
     return state.menuItems.sort((a, b) => ((a.category > b.category) ? 1 : -1));
   },
-/*   getOriginalMenuItem(state) {
+   getOriginalMenuItem(state) {
     if(state.itemToEdit !== null) {
       return state.menuItems.find(x => x.productName === state.itemToEdit.productName);
     } else {
       return [];
     }
-  }, */
+  },
 };
