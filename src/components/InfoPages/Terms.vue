ad37d50cdaa5ad4b9f32e6d1abe08cf2aed7aef0
sandraottersten <otterstensandra@gmail.com>
2020-02-09 11:51:37 +0100

    added terms

--- src/components/InfoPages/Terms.vue
+++ src/components/InfoPages/Terms.vue
@@ -1,6 +1,26 @@
 <template>
     <div class='terms'>
-      <h1>Terms Component</h1>
+      <h1>Villkor</h1>
+      <p>Genom att genomföra ett köp hos Thai Corner samtycker du till våra villkor. Thai Corner innehar rätten att ändra villkoren och därför rekommenderas kunder att informera sig om dessa innan köp.
+      Thai Corner reserverar sig för fel och oförutsedda händelser relaterade till produkttillgång, prisjusteringar, prisangivelser, produktbeskrivningar och tekniska problem. </p>
+      <br>
+      <h5>Betalning</h5>
+      <p>Thai Corner godkänner betalning genom större betalkort, administrerat genom Stripe. Genom att utge information vid betalning, godkänner kunden de villkor som föreskrivs av Stripe. </p>
+      <br>
+      <h5>Kvitto</h5>
+      <p>Kvitto på genomfört köp skickas till den emailadress som uppges vid betalning.</p>
+      <br>
+      <h5>Privacy Policy</h5>
+      <p>Personlig data är information som kan kopplas till en individ. In the context of One Dear Thing, personal data is defined as:
+      Email address
+      IP address
+      When visiting our web site and placing an order with us, we store data supplied with you. We use this data for the following purposes:
+      Kvitto
+      Payment handling
+      Bookkeeping as per Swedish legislation
+      We protect your privacy in relation to the above by never knowingly passing on your information to any other party. Your data is stored with us until you ask to have it removed.</p>
+      <br>
+      <p>Vänligen kontakta Thai Corner om du har några frågor, or wish to see or erase your data</p>
     </div>
 </template>
 
