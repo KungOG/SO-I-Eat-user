939c690b748ab36ad053a1f031083e89f1c415ba
Oscar Gustafsson <oscargustafsson@Dittes-iMac.local>
2020-02-13 14:11:55 +0100

    Cleaned payment function

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="payment">
     <div class="payment-background-overlay" @click="closePaymentModal" />
-    <img class='modal-cross-icon' :src="closeDown" />
-    <div class='payment-wrapper'>
-      <form id="payment-form" class='payment-form'>
-        <div class='payment-information'>
-          <div class='payment-error'>
-            <p class="stripeError"  v-if="stripeError">
-              {{stripeError}}
-            </p>
+    <img class="modal-cross-icon" :src="closeDown" />
+    <div class="payment-wrapper">
+      <form id="payment-form" class="payment-form">
+        <div class="payment-information">
+          <div class="payment-error">
+            <p class="stripeError" v-if="stripeError">{{stripeError}}</p>
           </div>
-          <div class='payment-card'>
-            <div class="" id="card-element"></div>
+          <div class="payment-card">
+            <div class id="card-element"></div>
           </div>
         </div>
-        <div class='payment-buttons'>
-          <button id='submit-button' @click.prevent="submitPayment()">
+        <div class="payment-buttons">
+          <button id="submit-button" @click.prevent="submitPayment()">
             <div class="spinner hidden" id="spinner"></div>
-            <span class='button-text'>Betala</span>
+            <span class="button-text">Betala</span>
           </button>
-          <button id='reset-button' @click.prevent="reset()">
-            <span class='button-text'>Återställ</span>
+          <button id="reset-button" @click.prevent="reset()">
+            <span class="button-text">Återställ</span>
           </button>
         </div>
       </form>
@@ -29,35 +27,35 @@
 </template>
 
 <script>
-import axios from 'axios';
-import CloseDown from '@/assets/icons/WhiteCross.svg';
+import axios from "axios";
+import CloseDown from "@/assets/icons/WhiteCross.svg";
 
 const style = {
   base: {
-    color: '#32325d',
+    color: "#32325d",
     fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4',
-    },
+    fontSmoothing: "antialiased",
+    fontSize: "16px",
+    "::placeholder": {
+      color: "#aab7c4"
+    }
   },
   invalid: {
-    color: '#AA0909',
-    iconColor: '#AA0909',
-  },
+    color: "#AA0909",
+    iconColor: "#AA0909"
+  }
 };
 
 export default {
   data: () => ({
     closeDown: CloseDown,
-    clientSecret: '',
+    clientSecret: "",
     card: null,
     stripe: null,
-    stripeError: '',
-    cardError: '',
+    stripeError: "",
+    cardError: "",
     cardEvent: null,
-    loading: false,
+    loading: false
   }),
   mounted() {
     this.createPaymentIntent();
@@ -67,52 +65,51 @@ export default {
       return this.$store.state.addOns;
     },
     itemsId() {
-      const order = this.$store.state.order;
-      const allAddOns = order.foodItems.map(x => x.add).flat()
-      const addOns = this.addOns;
-      let addOnIds = []
-
-      for (let i = 0; i< allAddOns.length; i++) {
-        for (let j = 0; j< addOns.length; j++) {
-          allAddOns[i] === addOns[j].name ? addOnIds.push(addOns[j]._id.toString()) : '';
-        }
-      }
+      const { order } = this.$store.state;
+      const { addOns } = this;
+      const allAddOns = order.foodItems.map(x => x.add).flat();
+      const addonsName = addOns.map(item => item.name);
       const foodId = order.foodItems.map(x => x._id);
       const drinkId = order.drinkItems.map(x => x._id);
+
+      const addOnIds = allAddOns.map(item => {
+        const arrPos = addonsName.indexOf(item);
+        return arrPos > -1 ? addOns[arrPos]._id.toString() : "";
+      });
       return [...drinkId, ...foodId, ...addOnIds];
-    },
+    }
   },
   methods: {
     createPaymentIntent() {
-      const url = 'https://so-i-eat-server.herokuapp.com/create-payment-intent';
-      const orderData = { items: this.itemsId, currency: 'sek' };
+      const url = "https://so-i-eat-server.herokuapp.com/create-payment-intent";
+      const orderData = { items: this.itemsId, currency: "sek" };
       axios
-        .post(url, orderData, { headers: { ContentType: 'application/json' } })
-        .then((response) => {
+        .post(url, orderData, { headers: { ContentType: "application/json" } })
+        .then(response => {
           this.clientSecret = response.data.clientSecret;
           this.setUpStripe(response.data);
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
     setUpStripe(data) {
       if (window.Stripe === undefined) {
-        alert('Stripe not loaded!');
+        alert("Stripe not loaded!");
       } else {
         const stripe = window.Stripe(data.publishableKey);
         this.stripe = stripe;
         const elements = stripe.elements();
-        this.card = elements.create('card', { style });
-        this.card.mount('#card-element');
+        this.card = elements.create("card", { style });
+        this.card.mount("#card-element");
         this.listenForErrors();
       }
     },
     listenForErrors() {
       const vm = this;
-      this.card.addEventListener('change', (event) => {
+      this.card.addEventListener("change", event => {
         vm.toggleError(event);
-        vm.cardError = '';
+        vm.cardError = "";
         vm.cardEvent = !!event.complete;
       });
     },
@@ -120,38 +117,40 @@ export default {
       if (event.error) {
         this.stripeError = event.error.message;
       } else {
-        this.stripeError = '';
+        this.stripeError = "";
       }
     },
     submitPayment() {
-      this.stripe.confirmCardPayment(this.clientSecret, {
-        payment_method: { card: this.card },
-      }).then((result) => {
-        if (result.error) {
-          this.stripeError = result.error.message;
-        } else if (result.paymentIntent.status === 'succeeded') {
-          console.log('betalningen gick igenom');
-          this.sendOrder();
-          // There's a risk of the customer closing the window before callback
-          // execution. Set up a webhook or plugin to listen for the
-          // payment_intent.succeeded event that handles any business critical
-          // post-payment actions.
-        }
-      });
+      this.stripe
+        .confirmCardPayment(this.clientSecret, {
+          payment_method: { card: this.card }
+        })
+        .then(result => {
+          if (result.error) {
+            this.stripeError = result.error.message;
+          } else if (result.paymentIntent.status === "succeeded") {
+            console.log("betalningen gick igenom");
+            this.sendOrder();
+            // There's a risk of the customer closing the window before callback
+            // execution. Set up a webhook or plugin to listen for the
+            // payment_intent.succeeded event that handles any business critical
+            // post-payment actions.
+          }
+        });
     },
     async sendOrder() {
-      await this.$store.dispatch('postOrder');
-      this.$router.push('/confirmation');
-      this.$store.commit('setShowPayment', false);
-      this.$store.commit('toggleActiveCart', false);
-      this.$store.commit('resetOrder');
+      await this.$store.dispatch("postOrder");
+      this.$router.push("/confirmation");
+      this.$store.commit("setShowPayment", false);
+      this.$store.commit("toggleActiveCart", false);
+      this.$store.commit("resetOrder");
     },
     clearElementsInputs() {
       this.card.clear();
     },
     clearCardErrors() {
-      this.stripeError = '';
-      this.cardError = '';
+      this.stripeError = "";
+      this.cardError = "";
     },
     reset() {
       this.clearElementsInputs();
@@ -159,7 +158,7 @@ export default {
     },
     closePaymentModal() {
       this.$store.state.showPayment = false;
-    },
-  },
+    }
+  }
 };
 </script>
